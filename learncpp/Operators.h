341606a4eadd0c681695b21a607f2bfcebd81c75
manuhilds <manuhilds@gmail.com>
2020-04-24 11:10:47 +0200

    Operator added

---
+++ learncpp/Operators.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "header_necessity.h"
+/*
+ *  operator   : symbol or pair of symbols specifying an operation between 2 operands
+ *  precedence : order in which operators are evaluated 
+ *  associativity    : tells in which direction to evalutate when 2 operators have the same precedence level (R->L),(L->R)
+ *  parenthesization : use of parenthesis to change the order ol application of operations
+ *  check list : "https://www.learncpp.com/cpp-tutorial/operator-precedence-and-associativity/"
+ */
+class Operators
+{
+
+};
+
